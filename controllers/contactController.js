ab2f57bda6b8d36bdbff5bfdb1e503031656aac2
Mekedelawit <mekedi.mkt@gmail.com>
2025-01-31 23:47:52 +0300

    Implement CRUD operations for contacts in contactController

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -19,28 +19,51 @@ const createContact = asyncHandler(async (req, res) => {
         res.status(400);
         throw new Error('Please provide name, email and phone');
     }
-    res.status(201).json({ 'message': 'Create contact' });
+    const contact = await Contact.create( {name, email, phone});
+    res.status(201).json(contact);
 });
 
 //@desc Get a contact
 //@route GET /api/contacts/:id
 //@access Public
 const getContact = asyncHandler(async (req, res) => {
-    res.status(200).json({ 'message': `Get contact for ${req.params.id}` });
+    const contact = await Contact.findById(req.params.id);
+    if (!contact) {
+        res.status(404);
+        throw new Error('Contact not found');
+    }
+    res.status(200).json(contact);
 });
 
 //@desc Update contact
 //@route PUT /api/contacts/:id
 //@access Public
 const updateContact = asyncHandler(async (req, res) => {
-    res.status(200).json({ 'message': `Update contact for ${req.params.id}` });
+    const contact = await Contact.findById(req.params.id);
+    if (!contact) {
+        res.status(404);
+        throw new Error('Contact not found');
+    }
+
+    const updatedContact = await Contact.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        { new: true }
+    )
+    res.status(200).json(updatedContact);
 });
 
 //@desc Delete contact
 //@route DELETE /api/contacts/:id
 //@access Public
 const deleteContact = asyncHandler(async (req, res) => {
-    res.status(200).json({ 'message': `Delete contact for ${req.params.id}` });
+    const contact = await Contact.findById(req.params.id);
+    if (!contact) {
+        res.status(404);
+        throw new Error('Contact not found');
+    }
+    await Contact.deleteOne({ _id: req.params.id}); // Contact.remove()
+    res.status(200).json(contact);
 });
 
 module.exports = { getContacts, createContact, getContact, updateContact, deleteContact };
