6384a02e9bc68c2853e621e11f942b8d879c2661
mekedi-mkt <mekedi.mkt@gmail.com>
2025-02-05 16:55:41 +0300

    Private authorized contact operations

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -3,67 +3,78 @@ const Contact = require("../models/contactModel");
 
 //@desc Get all contacts
 //@route GET /api/contacts
-//@access Public
+//@access private
 const getContacts = asyncHandler(async (req, res) => {
-    const contacts = await Contact.find();
-    res.status(200).json(contacts);
+  const contacts = await Contact.find({ user_id: req.user.id });
+  res.status(200).json(contacts);
 });
 
 //@desc Create new contact
 //@route POST /api/contacts
-//@access Public
+//@access private
 const createContact = asyncHandler(async (req, res) => {
-    console.log('The request body is: ', req.body);
-    const { name, email, phone } = req.body;
-    if (!name || !email || !phone) {
-        res.status(400);
-        throw new Error('Please provide name, email and phone');
-    }
-    const contact = await Contact.create( {name, email, phone});
-    res.status(201).json(contact);
+  console.log("The request body is: ", req.body);
+  const { name, email, phone } = req.body;
+  if (!name || !email || !phone) {
+    res.status(400);
+    throw new Error("Please provide name, email and phone");
+  }
+  const contact = await Contact.create({
+    name,
+    email,
+    phone,
+    user_id: req.user.id,
+  });
+  res.status(201).json(contact);
 });
 
 //@desc Get a contact
 //@route GET /api/contacts/:id
-//@access Public
+//@access private
 const getContact = asyncHandler(async (req, res) => {
-    const contact = await Contact.findById(req.params.id);
-    if (!contact) {
-        res.status(404);
-        throw new Error('Contact not found');
-    }
-    res.status(200).json(contact);
+  const contact = await Contact.findById(req.params.id);
+  if (!contact) {
+    res.status(404);
+    throw new Error("Contact not found");
+  }
+  res.status(200).json(contact);
 });
 
 //@desc Update contact
 //@route PUT /api/contacts/:id
-//@access Public
+//@access private
 const updateContact = asyncHandler(async (req, res) => {
-    const contact = await Contact.findById(req.params.id);
-    if (!contact) {
-        res.status(404);
-        throw new Error('Contact not found');
-    }
+  const contact = await Contact.findById(req.params.id);
+  if (!contact) {
+    res.status(404);
+    throw new Error("Contact not found");
+  }
 
-    const updatedContact = await Contact.findByIdAndUpdate(
-        req.params.id,
-        req.body,
-        { new: true }
-    )
-    res.status(200).json(updatedContact);
+  const updatedContact = await Contact.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    { new: true }
+  );
+  res.status(200).json(updatedContact);
 });
 
 //@desc Delete contact
 //@route DELETE /api/contacts/:id
-//@access Public
+//@access private
 const deleteContact = asyncHandler(async (req, res) => {
-    const contact = await Contact.findById(req.params.id);
-    if (!contact) {
-        res.status(404);
-        throw new Error('Contact not found');
-    }
-    await Contact.deleteOne({ _id: req.params.id}); // Contact.remove()
-    res.status(200).json(contact);
+  const contact = await Contact.findById(req.params.id);
+  if (!contact) {
+    res.status(404);
+    throw new Error("Contact not found");
+  }
+  await Contact.deleteOne({ _id: req.params.id }); // Contact.remove()
+  res.status(200).json(contact);
 });
 
-module.exports = { getContacts, createContact, getContact, updateContact, deleteContact };
+module.exports = {
+  getContacts,
+  createContact,
+  getContact,
+  updateContact,
+  deleteContact,
+};
